3f72c078fe4a62cc7c3288cb57b902fc70e0d6da
claireWei <claire610323@gmail.com>
2022-07-14 14:05:21 -0700

    style setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { createTheme } from "@material-ui/core";
+import { ThemeProvider } from '@material-ui/styles';
+import Routes from './Routes'
+import { Toaster } from "react-hot-toast";
+import './styles/_base.scss'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff6363',
+      dark: '#b24545',
+    },
+    secondary: {
+      main: '#f0c040',
+    },
+    text: {
+      primary: '#333',
+      secondary: '#666',
+    },
+    background: {
+      default: '#f7f7f7',
+    },
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightBold: 700,
+    fontWeightMedium: 500,
+    fontWeightRegular: 300,
+    htmlFontSize: 8,
+  },
+})
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Toaster />
+        <Routes />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
