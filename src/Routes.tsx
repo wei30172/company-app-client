6f97521763e0da458cb9744f6c72230114836042
claireWei <claire610323@gmail.com>
2022-07-18 17:22:53 -0700

    orders & data

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Home, Products } from "./pages";
+import { Home, Products, Orders } from "./pages";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
+      <Route path="/orders" element={<Orders />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
